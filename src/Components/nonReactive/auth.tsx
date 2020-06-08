abf72bfb6d8626ab7af5076e9b69929bba12b24f
OPtimal <jggsecondary@gmail.com>
2020-06-08 15:55:53 +0300

    Organize components folder

---
+++ src/Components/nonReactive/auth.tsx
@@ -0,0 +1,33 @@
+import { logonReturn, signUpReturn } from "../types";
+const axios = require("axios").default;
+
+export async function LogOn(username: string, password: string) {
+  let req: any;
+
+  try {
+    const hi = await axios.post(
+      "https://sx-alts-server.herokuapp.com/api/login",
+      {
+        username,
+        password,
+      }
+    );
+    req = hi.data.token;
+  } catch (err) {
+    console.log(err.response);
+    throw err.response.data.message;
+  }
+
+  return req;
+}
+
+export async function SignUp(username: string, password: string) {
+  try {
+    await axios.post("https://sx-alts-server.herokuapp.com/api/signup", {
+      username,
+      password,
+    });
+  } catch (err) {
+    throw err.response.data.message;
+  }
+}
---
+++ src/Components/nonReactive/getAlt.tsx
@@ -0,0 +1,22 @@
+import { signUpReturn } from "../types";
+import axios from "axios";
+
+export async function getAlt() {
+  const token = localStorage.getItem("token");
+  let req: any;
+
+  try {
+    const hi = await axios.get("https://sx-alts-server.herokuapp.com/api/alt", {
+      headers: {
+        token: token
+      }
+    });
+    req = hi.data.alt
+  } catch (err) {
+    console.log(err)
+    req = "";
+    throw err.response.data.message
+  }
+
+  return req
+}
