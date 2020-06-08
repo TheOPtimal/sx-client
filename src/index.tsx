a4fbedf68f703034d4bcbf913c514af14c10d6cb
OPtimal <jggsecondary@gmail.com>
2020-06-08 13:45:41 +0300

    Added logo

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,27 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Login from "./Pages/Login/Login";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Dashboard from "./Pages/Dashboard/dashboard";
 import Blacklisted from "./Pages/Blacklisted/blacklisted";
+import Logo from "./Images/sxalts.gif";
+import "./Logo.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Switch>
-        <Route path="/blacklisted"><Blacklisted /></Route>
-        <Route path="/dashboard"><Dashboard /></Route>
-        <Route path="/">
-          <Login />
-        </Route>
-      </Switch>
+      <Route path="/">
+        <img className="Logo" src={Logo} alt="S-X alts logo" />
+      </Route>
+      <Route exact path="/blacklisted">
+        <Blacklisted />
+      </Route>
+      <Route exact path="/dashboard">
+        <Dashboard />
+      </Route>
+      <Route exact path="/">
+        <Login />
+      </Route>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
