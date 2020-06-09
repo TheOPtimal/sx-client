1486c98cc131ca0317050cf00401e3e781591da6
OPtimal <jggsecondary@gmail.com>
2020-06-09 21:49:58 +0300

    Seperate clickAlt from Dashboard component and make autogen history

--- src/Pages/Dashboard/dashboard.tsx
+++ src/Pages/Dashboard/dashboard.tsx
@@ -5,19 +5,21 @@ import useInterval from "../../Components/useInterval";
 import Info from "../../Components/GeneralInfo";
 import BrowserInfo from "../../Components/GeneralInfo";
 
-export default function Dashboard() {
+function clickAlt(
+  genAlt: () => Promise<any>,
+  setCurAlt: React.Dispatch<React.SetStateAction<string>>
+): void {
+  getAlt()
+    .then((alt) => {
+      setCurAlt(alt);
+    })
+    .catch(alert);
+}
+
+export default React.memo(function Dashboard() {
   const [curAlt, setCurAlt] = useState("");
-  const [autoGen, setAutoGen] = useState<boolean>(false);
   const [intervalDelay, setIntervalDelay] = useState(2);
   const intervalRef = useRef(null);
-
-  function clickAlt(): void {
-    getAlt()
-      .then((alt) => {
-        setCurAlt(alt);
-      })
-      .catch(alert);
-  }
 
   useEffect(() => {
     if (!localStorage.getItem("token")) {
@@ -25,34 +27,30 @@ export default function Dashboard() {
     }
   }, []);
 
-  useEffect(() => {
-    if (autoGen) {
-      clickAlt();
-      intervalRef.current = setInterval(clickAlt, intervalDelay * 1000);
-    } else {
-      clearInterval(intervalRef.current);
-    }
-    return () => clearInterval(intervalRef.current);
-  }, [autoGen, intervalDelay]);
-
   return (
     <>
       <div className="dashboard">
         <LogOut />
         <AltBox curAlt={curAlt} />
-        <button onClick={clickAlt}>Generate alt</button>
+        <button onClick={() => clickAlt(getAlt, setCurAlt)}>
+          Generate alt
+        </button>
         <CopyToClipboard curAlt={curAlt} />
       </div>
       <BrowserInfo className="generalInfo" />
     </>
   );
-}
+});
 
 const AltBox = React.memo(function AltBox({ curAlt }: { curAlt: string }) {
   return <input type="text" value={curAlt} />;
 });
 
-const CopyToClipboard = React.memo(function CopyToClipboard({ curAlt }: { curAlt: string }) {
+const CopyToClipboard = React.memo(function CopyToClipboard({
+  curAlt,
+}: {
+  curAlt: string;
+}) {
   return (
     <button
       onClick={() =>
