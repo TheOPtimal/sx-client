55d14ea7d04164e2d626e5170a1df8cade348b27
OPtimal <jggsecondary@gmail.com>
2020-06-10 09:24:55 +0300

    Create clock

---
+++ src/Components/Clock.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import useInterval from "./useInterval";
+
+export default React.memo(function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useInterval(() => {
+    setTime(new Date());
+  }, 500);
+
+  return <div className="clock">{time.toLocaleTimeString()}</div>;
+});
